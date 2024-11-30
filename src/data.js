f18d078091b533670c48417536f1a7c3d33ec583
ieabbas <ieabbas@users.noreply.github.com>
2024-11-29 23:00:52 -0800

    Started work on getting boilerplate CSS going, and installed the deprecated babel dependency "plugin-propposal-private-property-in-object"

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const games = [
+    {
+      id: 1,
+      name: "Nier Replicant",
+      year: "2021",
+      image: "/images/nier-replicant.jpg",
+      link: "https://store.steampowered.com/app/1113560/NieR_Replicant/"
+    },
+    {
+      id: 2,
+      name: "Baba is You",
+      year: "2022",
+      image: "/images/baba-is-you.jpg",
+      link: "https://store.steampowered.com/app/736260/Baba_Is_You/"
+    },
+    // Add more games here
+  ];
+  
