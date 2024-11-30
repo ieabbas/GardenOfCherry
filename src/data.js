e36fff7ff34420ce0e3606de2a2605ad83637f9a
ieabbas <ieabbas@users.noreply.github.com>
2024-11-30 02:53:10 -0800

    Installed sharp library to resize images as needed for consistent sizing and resolution, also updated data.js to prep for example game covers to be imported into project locally

--- src/data.js
+++ src/data.js
@@ -1,17 +1,35 @@
 export const games = [
     {
       id: 1,
-      name: "Nier Replicant",
-      year: "2021",
-      image: "/images/nier-replicant.jpg",
-      link: "https://store.steampowered.com/app/1113560/NieR_Replicant/"
+      name: "Kingdom Hearts HD 1.5 + 2.5 ReMIX",
+      year: "2017",
+      releaseYear: "2017",
+      image: "/images/kh1525_cover.jpg",
+      link: "https://store.playstation.com/en-us/product/UP0082-CUSA05933_00-KINGDOMHEART1525"
     },
     {
       id: 2,
-      name: "Baba is You",
-      year: "2022",
-      image: "/images/baba-is-you.jpg",
-      link: "https://store.steampowered.com/app/736260/Baba_Is_You/"
+      name: "Bloodborne",
+      year: "2017",
+      releaseYear: "2015",
+      image: "/images/bloodborne_cover.jpg",
+      link: "https://store.playstation.com/en-us/product/UP9000-CUSA00900_00-BLOODBORNE000000"
+    },
+    {
+      id: 3,
+      name: "Outlast",
+      year: "2017",
+      releaseYear: "2013",
+      image: "/images/outlast_cover.jpg",
+      link: "https://store.steampowered.com/app/238320/Outlast/"
+    },
+    {
+      id: 4,
+      name: "Bioshock Infinite",
+      year: "2017",
+      releaseYear: "2013",
+      image: "/images/bioshockInfinite_cover.jpg",
+      link: "https://store.steampowered.com/app/8870/BioShock_Infinite/"
     },
     // Add more games here
   ];
