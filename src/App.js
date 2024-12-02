4f60e2fe247a9224a2719d14def6d2a9075f74c8
ieabbas <ieabbas@users.noreply.github.com>
2024-12-01 23:21:18 -0800

    Added a sub header describing the catalog, with hyperlinks to the Twitch and Youtube channels

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ const App = () => {
   return (
     <div className="webapp">
       <h1 style={{ textAlign: 'center' }}>THE GARDEN OF CHERRY</h1>
+      <h3 style={{ textAlign: 'center' }}>
+          A historical catalog of all the games streamed on   
+          <a href="https://twitch.tv/cherrius_" target="_blank" style={{ color: '#9146FF' }}> Twitch</a> or 
+          <a href="https://youtube.com/cherrius" target="_blank" style={{ color: '#FF0000' }}> Youtube</a>
+      </h3>
+
       <div className="app">
         <YearFilter
           years={["All", ...new Set(games.map(game => game.year))]} // Generate years dynamically as needed from data.js
